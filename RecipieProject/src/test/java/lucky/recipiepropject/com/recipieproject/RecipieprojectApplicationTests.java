5a8b91a10ca574e0ce7cf5c3c71d8509b982dc84
=SiddharthLucky <=siddharth.luckyreddy@gmail.com>
2018-07-19 11:33:07 -0400

    Initial commit
    Initial Commit

---
+++ RecipieProject/src/test/java/lucky/recipiepropject/com/recipieproject/RecipieprojectApplicationTests.java
@@ -0,0 +1,16 @@
+package lucky.recipiepropject.com.recipieproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipieprojectApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
